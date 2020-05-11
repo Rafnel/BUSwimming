2f4067fdc668d166fa39f653050b4efcab9048f4
Zachary Steudel <zacshack174@gmail.com>
2020-05-11 15:34:11 -0500

    fixed up profile page a bit. still ugly

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
+import { Typography } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 
 const ProfilePage = inject("userState")(observer(class ProfilePage extends React.Component {
     render(){
         let user = this.props.userState.user;
-
+        console.log(JSON.stringify(user));
         if(this.props.userState.loading){
             return(
                 <div>Loading...</div>
@@ -13,11 +16,23 @@ const ProfilePage = inject("userState")(observer(class ProfilePage extends React
         else{
             return(
                 <div style = {{margin: 10}}>
-                    <img src={user.picture} alt="Profile" />
+                    <Typography variant = "h5">
+                        Email: {user.email}
+                    </Typography>
 
-                    <h2>{user.name}</h2>
-                    <p>{user.email}</p>
-                    <code>{JSON.stringify(user, null, 2)}</code>
+                    <Typography variant = "h5">
+                        Email Verified: &nbsp;
+                        {user.email_verified && 
+                            <CheckIcon color = "primary"/>
+                        }
+                        {!user.email_verified && 
+                            <CloseIcon/>
+                        }
+                    </Typography>
+
+                    <Typography variant = "h5">
+                        Role: {user.role}
+                    </Typography>
                 </div>
             )
         }
